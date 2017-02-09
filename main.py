97f608fbf3e65e04445c143c963eb843d744758c
jbelamor <j.belena@alumnos.upm.es>
2017-02-09 19:55:47 +0100

    versión para entregar

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import zipfile
 # El proceso de intercambiado es: se coloca en una posicion aleatoria de las letras y se intercambia de forma aleatoria
 # con otra posicion aleatoria si calculando el timestamp con el modulo de la fecha de creacion de linux
 # (25 de agosto de 1991 = 25081991), se suman todas las cifras hasta que quede un numero y si ese numero es multiplo
-# de los 4 elementos (fuego, tierra, agua, aire, eter) entonces intercambia con otra letra aleatoria de la palabra
+# de 7, el número mágico. Entonces intercambia con otra letra aleatoria de la palabra
 
 linuxCreationTime = 25081991
 conditionChange = 7  # elemntos basicos
@@ -72,9 +72,6 @@ def calcCondition():
 
 
 def twistWord(word):
-    if word == 'Çeşme':
-        print('----------------------MEGAMIAU--------------------')
-        print(word.encode('utf-8', 'surrogatepass'))
     if len(word) < 3:
         return word
     #print(word)
@@ -161,10 +158,10 @@ def twistDocuments(file, extension):
         with myzip.open(document) as fileInRaw:
             textFile = fileInRaw.read().decode('utf-8')
             for key in dictOcurrences.keys():
-                print(key)
-                print(dictOcurrences[key])
+                #print(key)
+                #print(dictOcurrences[key])
                 textFile = textFile.replace(key, dictOcurrences[key])
-            print(textFile)
+            #print(textFile)
             fileInRaw.close()
         myzip.close()
         updateZip(file, document, textFile)
@@ -174,32 +171,22 @@ def searchTextFiles():
     OS = platform.system()
     rootDir = '.'
     if OS == 'Windows':
-        #os.chdir('USERPROFILE')
-        os.chdir('C:\\Users\\Joel\\Proyectos\\dislexic-program')
+        os.chdir('USERPROFILE')
     elif OS == 'Linux':
-        #os.chdir(os.getenv('HOME'))
-        pass
+        os.chdir(os.getenv('HOME'))
     for dirName, subdirList, fileList in os.walk(rootDir):
         #print(dirName)
         for fname in fileList:
             #print(fname)
             finalFile = os.path.join(dirName, fname)
-            if fname.split('.')[-1].lower() == 'txti':
-                #twistText(finalFile)
-                pass
+            if fname.split('.')[-1].lower() == txt:
+                print(finalFile)
+                twistText(finalFile)
             elif fname.split('.')[-1].lower() == docx:
                 print(finalFile)
                 twistDocuments(finalFile, docx)
             elif fname.split('.')[-1].lower() == odt:
                 print(finalFile)
                 twistDocuments(finalFile, odt)
-
-# for arg in sys.argv:
-#     if arg == '-h' or '--help':
-#         print('Usage {}')
-#     elif arg == '--sorrynotsorry' or '-sns':
-#         print('')
-#     elif arg == '-home':
-#         pass
 
 searchTextFiles()
